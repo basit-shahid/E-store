fa517bf591726c1a55daaba421b2152e955dde50
basit shahid <basitdogar59@gmail.com>
2026-07-24 20:59:26 +0500

    added models

--- database/migrations/2026_07_23_141018_create_products_table.php
+++ database/migrations/2026_07_23_141018_create_products_table.php
@@ -4,16 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
+
             $table->id();
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name');
+
+            $table->decimal('price', 8, 2);
+
+            $table->integer('stock');
+
             $table->timestamps();
+
         });
     }
 
